5eb16ae9fe0bb02d94157e4a0397c38dd2af06a6
son <wnstnals2007@naver.com>
2026-05-20 13:49:50 +0900

    検索画面作る

---
+++ src/main/java/com/jsl24/controller/CastingController.java
@@ -0,0 +1,15 @@
+package com.jsl24.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CastingController {
+
+    @GetMapping("/casting/project")
+    public String projectSearch(Model model) {
+        model.addAttribute("content", "casting/project :: projectContent");
+        return "layout";
+    }
+}
